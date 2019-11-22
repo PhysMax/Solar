39f16c4be5f7b7783894c92008714caa2fe6bbf1
PhysMax <prutovykh.mm@phystech.edu>
2019-11-22 13:12:06 +0300

    Initial commit
    Write README, add creating the window, add class space_obj, for it write __init__(), move(), apply_force(), screen_coords().

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+root = tk.Tk()
+
+window_width = 800
+window_height = 800
+
+space = tk.Canvas(root, width=window_width, height=window_height, bg="black")
+
+tk.mainloop()
---
+++ space_obj.py
@@ -0,0 +1,31 @@
+""" Contains class space_obj, parent class for space objects such as asteroids, planets, etc"""
+
+
+class space_obj:
+    def __init__(self, m, x, y, vx, vy, size, player):
+        self.m = m
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.size = size
+        self.player = player
+        self.force_x = 0
+        self.force_y = 0
+
+    def screen_coords(self):
+        return self.x - self.player.x, self.y - self.player.y
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+    def move(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+        self.vx = self.force_x / self.m * dt
+        self.vy = self.force_y / self.m * dt
+
+    def apply_force(self, force_x, force_y):
+        self.force_x += force_x
+        self.force_y += force_y
